e763d034ce7b2626e310bf9b4c1c28fcf4c37d5c
Daniel <daniel@Daniels-MacBook-Pro-2.local>
2023-11-25 23:59:43 +0100

    fixed

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from nltk.stem import PorterStemmer
 
 app = Flask(__name__)
 
-# Step 3: Read and Parse Documentation
 docs_folder = "DTIT TARDIS AI ChatBot -- Hackathon Kosice 2023/customer_handbook/docs"
 
 def read_docs(folder):
@@ -21,7 +20,6 @@ def read_docs(folder):
 
 documents = read_docs(docs_folder)
 
-# Step 4: Tokenization and Text Processing
 nlp = spacy.load("en_core_web_sm")
 ps = PorterStemmer()
 
@@ -31,14 +29,11 @@ def preprocess_text(text):
 
 processed_docs = [preprocess_text(doc) for doc in documents]
 
-# Step 5: Build a Knowledge Base
 knowledge_base = dict(zip(map(tuple, processed_docs), documents))
 
-# Step 7: Answer Retrieval
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform([" ".join(doc) for doc in processed_docs])
 
-# History to store chat interactions
 chat_history = []
 
 def get_most_similar(query_vector):
@@ -47,19 +42,17 @@ def get_most_similar(query_vector):
     return knowledge_base[tuple(processed_docs[most_similar_index])]
 
 def extract_relevant_info(document, query):
-    sentences = document.split(". ")  # Split the document into sentences
+    sentences = document.split(". ")  
     query_vector = vectorizer.transform([" ".join(preprocess_text(query))])
     similarity_scores = cosine_similarity(query_vector,
                                           vectorizer.transform([" ".join(preprocess_text(sent)) for sent in sentences]))
 
-    # Set a higher threshold for similarity scores to filter out less relevant answers
-    threshold = 0.1  # Adjust this threshold based on your needs
+    threshold = 0.1 
 
-    # Check if there are any sentences in the document
+  
     if len(sentences) == 0:
         return "I can't answer this question."
 
-    # Find the index of the most similar sentence with a score above the threshold
     valid_indices = [i for i, score in enumerate(similarity_scores[0]) if score > threshold]
 
     if not valid_indices:
@@ -67,46 +60,36 @@ def extract_relevant_info(document, query):
 
     most_similar_sentence_index = valid_indices[0]
 
-    # Check if the index is within the valid range
     if 0 <= most_similar_sentence_index < len(sentences):
         return sentences[most_similar_sentence_index]
     else:
         return "I can't answer this question."
 
-# Intent Recognition
 def extract_intent(text):
     doc = nlp(text)
-    return doc.cats  # Assuming you have a model trained for intent classification
+    return doc.cats 
 
-# Named Entity Recognition (NER)
 def extract_entities(text):
     doc = nlp(text)
     entities = [(ent.text, ent.label_) for ent in doc.ents]
     return entities
 
-# Flask routes
 @app.route('/')
 def index():
-    reversed_chat_history = chat_history[::-1]  # Reverse the order
+    reversed_chat_history = chat_history[::-1] 
     return render_template('index.html', chat_history=reversed_chat_history)
 
-# Add other routes as needed, such as image display
-
-# Chat route
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.form['user_input']
 
-    # Extract intent and entities
     intent = extract_intent(user_input)
     entities = extract_entities(user_input)
 
-    # Process text as usual if not an image
     processed_query = preprocess_text(user_input)
     query_vector = vectorizer.transform([" ".join(processed_query)])
     most_similar_document = get_most_similar(query_vector)
 
-    # Extract relevant information from the document based on the user's query
     relevant_info = extract_relevant_info(most_similar_document, user_input)
 
     chat_history.append({'user_input': user_input, 'intent': intent, 'entities': entities, 'answer': relevant_info})
